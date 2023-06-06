e45d7022e47bf3160d1f5b530e59498f0929356e
geethuelizebeth <geethuelizebeth@gmail.com>
2023-06-06 20:34:02 +0100

    msg

--- CodingTrain/src/com/gems/codingtrain/java8/functional/lamdaexpression.java
+++ CodingTrain/src/com/gems/codingtrain/java8/functional/lamdaexpression.java
@@ -5,7 +5,6 @@ import java.util.List;
 public class lamdaexpression {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		List<Integer> n=List.of(1,2,3,4,5);
 		n.stream().filter(n1->n1%2==0).forEach(System.out::println);
 		int max=n.stream().max((n1,n2)->Integer.compare(n1, n2)).get();
